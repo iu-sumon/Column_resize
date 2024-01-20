e6be35155d6931a9d35a9dacdd928264238c4097
Sumon <md.sumonmia.cse.iu@gmail.com>
2024-01-20 20:12:01 +0600

    Column resize done

---
+++ app.js
@@ -0,0 +1,14 @@
+
+    // $("table").resizableColumns({
+    //   store: window.store,
+    //   minWidth: 15
+    //   //resize: onResize
+    // });
+
+
+    $("#wl_table_1").resizableColumns({
+        store: window.store,
+        minWidth: 15
+        //resize: onResize
+      });
+      
